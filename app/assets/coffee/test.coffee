d7a1ff0631b683a908fcba6c7c4a5949be68037b
Alexey Petrov <apetrov@techranch.ru>
2014-12-16 19:00:23 +0400

    show inputs

--- app/assets/coffee/test.coffee
+++ app/assets/coffee/test.coffee
@@ -1,12 +1,38 @@
 app = angular.module "testApp", []
 
-app.run ($rootScope) ->
-  $rootScope.model = {}
-  $rootScope.model.variable = "Hello"
-  return
+app.directive "rangeParser", ($log) ->
+  return {
+    require: "?ngModel"
+    link: (scope, element, attr, ctrl) ->
+      return unless ctrl?
+      ctrl.$parsers.push( (value) ->
+        val = Number(value)
+        val = null if val != val
+        return val
+      )
+  }
 
-app.controller "MainCtrl", ($scope) ->
-  $scope.changeVal = () ->
-    $scope.model.variable += " test"
+app.controller "MainCtrl", ($scope, $window) ->
+  $scope.model = {}
+  $window.model = $scope.model
+
+  $scope.data =
+    items: [
+      name: "item1"
+      percent: 0
+    ,
+      name: "item2"
+      percent: 0
+    ,
+      name: "item3"
+      percent: 0
+    ]
+
+  $scope.model = $scope.data.items
+  console.log $scope.model
+
+  $scope.changeVal = (item)->
+    console.log item
+    return
 
   return
--- app/build/js/test.js
+++ app/build/js/test.js
@@ -2,14 +2,46 @@ var app;
 
 app = angular.module("testApp", []);
 
-app.run(function($rootScope) {
-  $rootScope.model = {};
-  $rootScope.model.variable = "Hello";
+app.directive("rangeParser", function($log) {
+  return {
+    require: "?ngModel",
+    link: function(scope, element, attr, ctrl) {
+      if (ctrl == null) {
+        return;
+      }
+      return ctrl.$parsers.push(function(value) {
+        var val;
+        val = Number(value);
+        if (val !== val) {
+          val = null;
+        }
+        return val;
+      });
+    }
+  };
 });
 
-app.controller("MainCtrl", function($scope) {
-  $scope.changeVal = function() {
-    return $scope.model.variable += " test";
+app.controller("MainCtrl", function($scope, $window) {
+  $scope.model = {};
+  $window.model = $scope.model;
+  $scope.data = {
+    items: [
+      {
+        name: "item1",
+        percent: 0
+      }, {
+        name: "item2",
+        percent: 0
+      }, {
+        name: "item3",
+        percent: 0
+      }
+    ]
+  };
+  $scope.model = $scope.data.items;
+  console.log($scope.model);
+  $scope.changeVal = function(item) {
+    console.log(item);
   };
 });
 
